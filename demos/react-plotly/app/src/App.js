49fc09eb7614d115f42b508594e543e46cec6de3
odeimaiz <maiz@zurichmedtech.com>
2018-01-16 10:18:31 +0100

    minor

--- demos/react-plotly/app/src/App.js
+++ demos/react-plotly/app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h2 className="App-title">D3 within React</h2>
+          <h2 className="App-title">Plotly within React</h2>
         </header>
         <div width={'500px'}>
           <PlotlyPlot size={[500,500]} />
