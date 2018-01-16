6b40febfa9ae57a9d514d7aa0e65760db9cf20d3
odeimaiz <maiz@zurichmedtech.com>
2018-01-16 13:39:58 +0100

    minor

--- demos/react-plotly/app/src/components/PlotlyPlot.js
+++ demos/react-plotly/app/src/components/PlotlyPlot.js
@@ -10,18 +10,18 @@ class PlotlyPlot extends Component {
   render () {
     let myArray = [
       {
-        type: 'scatter',  // all "scatter" attributes: https://plot.ly/javascript/reference/#scatter
-        x: [1, 2, 3],     // more about "x": #scatter-x
-        y: [6, 2, 3],     // #scatter-y
-        marker: {         // marker is an object, valid marker keys: #scatter-marker
-          color: 'rgb(16, 32, 77)' // more about "marker.color": #scatter-marker-color
+        type: 'scatter',
+        x: [1, 2, 3],
+        y: [6, 2, 3],
+        marker: {
+          color: 'rgb(16, 32, 77)'
         }
       },
       {
-        type: 'bar',      // all "bar" chart attributes: #bar
-        x: [1, 2, 3],     // more about "x": #bar-x
-        y: [6, 2, 3],     // #bar-y
-        name: 'bar chart example' // #bar-name
+        type: 'bar',
+        x: [1, 2, 3],
+        y: [6, 2, 3],
+        name: 'bar chart example'
       }
     ];
     return (
