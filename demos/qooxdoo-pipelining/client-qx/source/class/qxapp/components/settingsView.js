e3e46d989b476f8340f6d2f3cdd4ba843ee8f8e3
odeimaiz <maiz@zurichmedtech.com>
2018-04-18 14:37:55 +0200

    Metadata settings to form

--- demos/qooxdoo-pipelining/client-qx/source/class/qxapp/components/settingsView.js
+++ demos/qooxdoo-pipelining/client-qx/source/class/qxapp/components/settingsView.js
@@ -57,7 +57,10 @@ qx.Class.define('qxapp.components.settingsView',
     },
 
     SetNodeMetadata: function(nodeMetadata) {
+      console.log(nodeMetadata.GetMetaData());
+
       this._settingsBox.removeAll();
+
       let changeTitleBox = new qx.ui.container.Composite(new qx.ui.layout.HBox(10));
       {
         let titleLabel = new qx.ui.basic.Label('Node Title');
@@ -70,6 +73,74 @@ qx.Class.define('qxapp.components.settingsView',
         changeTitleBox.add(titleInput, {width: '50%'});
       }
       this._settingsBox.add(changeTitleBox);
+
+      let form = new qx.ui.form.Form();
+      for (let i = 0; i < nodeMetadata.GetMetaData().settings.length; i++) {
+        let sett = nodeMetadata.GetMetaData().settings[i];
+        let input = this._fromMetadataToQxSetting(sett);
+        if (input) {
+          form.add(input, sett.text, null, sett.name);
+        }
+      }
+
+      // form with Compute and reset button
+      let computeButton = new qx.ui.form.Button('Save');
+      form.addButton(computeButton);
+      let resetButton = new qx.ui.form.Button('Reset');
+      form.addButton(resetButton);
+
+      let controller = new qx.data.controller.Form(null, form);
+      let model = controller.createModel();
+
+      computeButton.addListener('execute', function() {
+        if (form.validate()) {
+          for (let i = 0; i < nodeMetadata.GetMetaData().settings.length; i++) {
+            let settKey = nodeMetadata.GetMetaData().settings[i].name;
+            nodeMetadata.GetMetaData().settings[i].value = model.get(settKey);
+          }
+        }
+      }, this);
+
+      resetButton.addListener('execute', function() {
+        form.reset();
+      }, this);
+
+      this._settingsBox.add(new qx.ui.form.renderer.Single(form));
+    },
+
+    _fromMetadataToQxSetting: function(metadata) {
+      let input;
+      switch (metadata.type) {
+        case 'number':
+          input = new qx.ui.form.Spinner();
+          input.set({
+            value: metadata.value,
+          });
+          break;
+        case 'text':
+          input = new qx.ui.form.TextField();
+          input.set({
+            value: metadata.value,
+          });
+          break;
+        case 'select':
+          input = new qx.ui.form.SelectBox();
+          for (let j = 0; j < metadata.options.length; j++) {
+            let optionItem = new qx.ui.form.ListItem(metadata.options[j], null, j);
+            input.add(optionItem);
+          }
+          break;
+        case 'boolean':
+          input = new qx.ui.form.CheckBox();
+          input.set({
+            value: (metadata.value === 1),
+          });
+          break;
+        default:
+          input = null;
+          break;
+      }
+      return input;
     },
   },
 });
